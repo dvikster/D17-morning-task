f9150592d70334ffd7a7b7895d72b7b24877c542
dvikster <dvikster@gmail.com>
2017-05-16 10:03:38 +0300

    added files

--- js.js
+++ js.js
@@ -6,21 +6,24 @@ var trigBlock = document.querySelector('.trig_block');
 
 trigWrapp.onclick = trigPos;
 
-var key=1;
+var key=0;
+console.log(key);
+
 function trigPos(){
+    key++;
     if(key==1){
         trigBlock.style.marginLeft = '50px';
-        console.log('1');
+        console.log(key);
     }
     else if(key==2) {
         trigBlock.style.marginLeft = '100px';
-        console.log('2');
+        console.log(key);
     }
     else if(key==3) {
-        trigBlock.style.marginLeft = '0';
-        console.log('0');
         key=0;
+        trigBlock.style.marginLeft = '0';
+        console.log(key);
     }
-    key++;
+
 }
 
